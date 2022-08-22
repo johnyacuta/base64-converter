073baf29f9b88319c8ab9bdb3dae3e318fbd67ee
johnsk8 <Ludin@DESKTOP-BAB0NU4.(none)>
2022-08-21 20:40:15 -0700

    Added unit test for encode

--- tests/test_base64_converter.py
+++ tests/test_base64_converter.py
@@ -1,5 +1,17 @@
 from base64_converter import __version__
+from typer.testing import CliRunner
+from base64_converter.main import app
+
+
+runner = CliRunner()
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_base64_encode():
+    result = runner.invoke(app, ["e", "test"])
+    print("Result:", result.stdout)
+    assert result.exit_code == 0
+    assert "dGVzdA==" in result.stdout
